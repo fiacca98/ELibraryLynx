04c368361c40e343955b8bc4c3b4a4d7ba850cc0
fiacca98 <lui.gi98@live.it>
2018-12-06 17:38:57 +0100

    java classes

---
+++ src/org/etlabora/dao/DB.java
@@ -0,0 +1,17 @@
+package org.etlabora.dao;
+
+import java.sql.*;
+
+public class DB {
+    public static Connection getCon() {
+	Connection con = null;
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud_tables", "root", "root");
+
+	} catch (Exception e) {
+	    System.out.println(e);
+	}
+	return con;
+    }
+}
