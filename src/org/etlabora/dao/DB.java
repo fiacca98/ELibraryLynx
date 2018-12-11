e540b4e0ad01267a5a70f183cea83f1915c17165
Studente <Studente@DELL17-02>
2018-12-11 15:14:17 +0100

    ora funonzia tutto

--- src/org/etlabora/dao/DB.java
+++ src/org/etlabora/dao/DB.java
@@ -1,13 +1,14 @@
 package org.etlabora.dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DB {
     public static Connection getCon() {
 	Connection con = null;
 	try {
 	    Class.forName("com.mysql.jdbc.Driver");
-	    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/crud_tables", "root", "root");
+	    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/elibrary", "root", "root");
 
 	} catch (Exception e) {
 	    System.out.println(e);
